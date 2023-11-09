8f34caed45e5ccb5ae7003c377fbe4abc032fad5
Zachary Honey <zhoney466@gmail.com>
2023-11-08 20:08:44 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -3,7 +3,8 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require('./users/users-router')
 const authRouter = require('./auth/auth-router')
-
+const session = require('express-session')
+const store = require('connect-session-knex')(session)
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
