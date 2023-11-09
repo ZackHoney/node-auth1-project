ad5f9ab4060d30490d8a6b80ae493880a4d82726
Zachary Honey <zhoney466@gmail.com>
2023-11-09 11:56:38 -0800

    completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -88,7 +88,23 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
   }
  */
   router.get('/logout', (req, res, next ) => {
-    res.json('logout')
+    if (req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          next(err)
+        } else {
+          res.json({
+            status: 200,
+            message: 'logged out'
+          })
+        }
+      })
+    } else {
+      res.json({
+        status: 200, 
+        message: 'no session'
+      })
+    }
   })
 
  
